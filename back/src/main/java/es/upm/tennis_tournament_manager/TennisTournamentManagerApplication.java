a15cb890ab353f739f83b77a981ede44010cae7d
allgaleano <all.galeano.diaz@gmail.com>
2024-09-17 19:02:16 +0200

    Initial commit

---
+++ back/src/main/java/es/upm/tennis_tournament_manager/TennisTournamentManagerApplication.java
@@ -0,0 +1,13 @@
+package es.upm.tennis_tournament_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisTournamentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisTournamentManagerApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/es/upm/tennis_tournament_manager/TennisTournamentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.tennis_tournament_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisTournamentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
