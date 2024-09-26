c95e9006b9c9fd91c7db3722d08d875c9cb45031
allgaleano <all.galeano.diaz@gmail.com>
2024-09-27 01:13:08 +0200

    UserDetails custom implementation

--- back/src/main/java/es/upm/tennis/tournament/manager/repo/UserRepository.java
+++ back/src/main/java/es/upm/tennis/tournament/manager/repo/UserRepository.java
@@ -1,7 +1,10 @@
 package es.upm.tennis.tournament.manager.repo;
 
-import es.upm.tennis.tournament.manager.model.User;
+import es.upm.tennis.tournament.manager.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+@Repository
+public interface UserRepository extends JpaRepository<Users, Long> {
+    Users findByUsername(String username);
 }
