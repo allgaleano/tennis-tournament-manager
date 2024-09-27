4bb434ea0e6de3f511c99845b71215d171c43e56
allgaleano <all.galeano.diaz@gmail.com>
2024-09-27 19:54:10 +0200

    JWT auth implemented

---
+++ back/src/main/java/es/upm/tennis/tournament/manager/service/UserService.java
@@ -0,0 +1,39 @@
+package es.upm.tennis.tournament.manager.service;
+
+import es.upm.tennis.tournament.manager.model.Users;
+import es.upm.tennis.tournament.manager.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    AuthenticationManager authManager;
+
+    @Autowired
+    private JwtService jwtService;
+
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
+    public Users register(Users user) {
+        user.setPassword(encoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public String verify(Users user) {
+        Authentication authentication =
+                authManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        if(authentication.isAuthenticated())
+            return jwtService.generateToken(user.getUsername());
+        return "Fail";
+    }
+}
