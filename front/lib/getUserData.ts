e2b31102c0db44a7c3d64825abe44e7e8941b7ce
allgaleano <all.galeano.diaz@gmail.com>
2024-10-04 18:45:21 +0200

    Middleware route protection functionality implemented and getUserData() function to validate session with the Spring API

--- front/lib/getUserData.ts
+++ front/lib/getUserData.ts
@@ -1,3 +1,27 @@
-export const getUserData = () => {
+import { cookies } from "next/headers";
 
+export const getUserData = async () => {
+    const sessionId = cookies().get('Session-Id');
+    let user =  null;
+
+    if (!sessionId) {
+        return null;
+    }
+    try {
+        const res = await fetch(`${process.env.API_URI}/session`, {
+            method: 'GET',
+            headers: {
+                'Content-Type' : 'application/json',
+                'Session-Id' : sessionId.value
+            },
+        }); 
+
+        if (!res.ok) {
+            throw new Error(`Failed to fetch: ${res.status} ${res.statusText}`);
+        }
+        user = await res.json(); 
+    } catch (error) {
+        console.error("Fetch failed: ", error);
+    }
+    return user;
 }
