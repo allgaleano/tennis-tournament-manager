13c6e7eff840e9d4ca41b765db4f61098571b6f0
allgaleano <all.galeano.diaz@gmail.com>
2024-10-14 13:58:36 +0200

    hotfix: difference between timezones in docker container fixed changing LocalDateTime to Instant in the server

--- back/src/main/java/es/upm/tennis/tournament/manager/model/UserSession.java
+++ back/src/main/java/es/upm/tennis/tournament/manager/model/UserSession.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 
 @Entity
 @Getter
@@ -25,5 +25,5 @@ public class UserSession {
     private String sessionId;
 
     @Temporal(TemporalType.TIMESTAMP)
-    private LocalDateTime expirationDate;
+    private Instant expirationDate;
 }
