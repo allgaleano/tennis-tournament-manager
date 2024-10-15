b8016755531315c6923c4384cf29673f9d1ee9fa
allgaleano <all.galeano.diaz@gmail.com>
2024-10-15 20:35:47 +0200

    Side bar implemented

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,18 +1,5 @@
-import Dashboard from "@/components/dashboard";
-import { getUserData } from "@/lib/getUserData";
-import BarLoader from "react-spinners/BarLoader";
-
 export default async function Home() {
-  const userData = await getUserData();
   return (
-    <div>
-      {userData ? (
-        <Dashboard userData={userData} />
-      ) : (
-        <div className="w-full h-screen flex justify-center items-center">
-          <BarLoader />
-        </div>
-      )}
-    </div>
+    <div></div>
   );
 }
