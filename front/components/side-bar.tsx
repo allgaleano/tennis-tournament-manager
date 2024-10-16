c0fc9b512154474ea736cdf5deef86cc6fa6c528
allgaleano <all.galeano.diaz@gmail.com>
2024-10-16 18:27:57 +0200

    MatchPoint logo added

--- front/components/side-bar.tsx
+++ front/components/side-bar.tsx
@@ -8,10 +8,11 @@ import { usePathname, useRouter } from 'next/navigation';
 import { BiSolidTennisBall } from "react-icons/bi";
 import { FaUserShield } from "react-icons/fa6";
 import { TbLogout2 } from "react-icons/tb";
-import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from '@/components/ui/sheet';
 import { FiMenu } from "react-icons/fi";
 import { UserData } from '@/types';
 import { MdInsights } from "react-icons/md";
+import Image from 'next/image';
 
 const SideBar = ({ userData }: { userData: UserData }) => {
   const { toast } = useToast();
@@ -53,6 +54,9 @@ const SideBar = ({ userData }: { userData: UserData }) => {
   return (
     <div>
       <div className="hidden md:flex flex-col h-screen p-4 border-r min-w-[250px] gap-2">
+        <div className="w-full flex items-center justify-center p-4">
+          <Image src="/static/Logotipo_MatchPoint.svg" width={200} height={25} alt="Match Point"/>
+        </div>
         <Button
           variant={pathname === "/dashboard" ? "outline" : "ghost"}
           className="font-semibold justify-start gap-2"
@@ -93,15 +97,20 @@ const SideBar = ({ userData }: { userData: UserData }) => {
             </Button>
           </SheetTrigger>
           <SheetContent side="left" className="flex flex-col bg-white pt-16">
+            <SheetHeader>
+              <div className="w-full flex items-center justify-center pb-4">
+                <Image src="/static/Logotipo_MatchPoint.svg" width={200} height={25} alt="Match Point" />
+              </div>
+            </SheetHeader>
             <Button
               variant={pathname === "/dashboard" ? "outline" : "ghost"}
-              className="font-semibold justify-start gap-2"
+              className="font-semibold justify-start gap-2 py-6"
             >
               <MdInsights /> Dashboard
             </Button>
             <Button
               variant={pathname === "/tournaments" ? "outline" : "ghost"}
-              className="font-semibold justify-start gap-2"
+              className="font-semibold justify-start gap-2 py-6"
             >
               <BiSolidTennisBall /> Torneos
             </Button>
