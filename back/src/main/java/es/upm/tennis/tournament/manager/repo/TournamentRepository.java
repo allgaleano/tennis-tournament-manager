cdb5ce7fa591af0023d68abee1334ffa608d238c
allgaleano <all.galeano.diaz@gmail.com>
2024-10-29 16:40:21 +0100

    feat: added tournaments initializer and field of is_open to tournaments table

--- back/src/main/java/es/upm/tennis/tournament/manager/repo/TournamentRepository.java
+++ back/src/main/java/es/upm/tennis/tournament/manager/repo/TournamentRepository.java
@@ -4,6 +4,9 @@ import es.upm.tennis.tournament.manager.model.Tournament;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TournamentRepository extends JpaRepository<Tournament, Long> {
+    Optional<Tournament> findByName (String name);
 }
---
+++ back/src/main/java/es/upm/tennis/tournament/manager/utils/TournamentsInitializer.java
@@ -0,0 +1,50 @@
+package es.upm.tennis.tournament.manager.utils;
+
+import es.upm.tennis.tournament.manager.model.Tournament;
+import es.upm.tennis.tournament.manager.repo.TournamentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@Component
+public class TournamentsInitializer implements CommandLineRunner {
+
+    @Autowired
+    TournamentRepository tournamentRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        if (tournamentRepository.findByName("spring").isEmpty()) {
+            Tournament tournament = new Tournament();
+            tournament.setName("spring");
+            tournament.setRegistrationDeadline(ZonedDateTime.of(2025, 3, 19, 23, 59, 0, 0, ZoneId.of("UTC")).toInstant());
+            tournamentRepository.save(tournament);
+        }
+
+        if (tournamentRepository.findByName("summer").isEmpty()) {
+            Tournament tournament = new Tournament();
+            tournament.setName("summer");
+            tournament.setRegistrationDeadline(ZonedDateTime.of(2025, 6, 20, 23, 59, 0, 0, ZoneId.of("UTC")).toInstant());
+            tournamentRepository.save(tournament);
+        }
+
+        if (tournamentRepository.findByName("autumn").isEmpty()) {
+            Tournament tournament = new Tournament();
+            tournament.setName("autumn");
+            tournament.setRegistrationDeadline(ZonedDateTime.of(2025, 8, 31, 23, 59, 0, 0, ZoneId.of("UTC")).toInstant());
+            tournamentRepository.save(tournament);
+        }
+
+        if (tournamentRepository.findByName("winter").isEmpty()) {
+            Tournament tournament = new Tournament();
+            tournament.setName("winter");
+            tournament.setRegistrationDeadline(ZonedDateTime.of(2025, 12, 20, 23, 59, 0, 0, ZoneId.of("UTC")).toInstant());
+            tournamentRepository.save(tournament);
+        }
+    }
+}
