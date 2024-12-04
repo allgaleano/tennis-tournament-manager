d121ad7b6f1deabd829c812bcce20b97fc2a6fdf
allgaleano <all.galeano.diaz@gmail.com>
2024-12-04 14:20:20 +0100

    feat: recursivity in MatchmakingService to create future rounds

--- back/src/main/java/es/upm/tennis/tournament/manager/DTO/FirstRoundMatchmakingResult.java
+++
@@ -1,6 +0,0 @@
-package es.upm.tennis.tournament.manager.DTO;
-
-import es.upm.tennis.tournament.manager.model.TournamentRound;
-import es.upm.tennis.tournament.manager.model.User;
-
-public record FirstRoundMatchmakingResult(User playerWithBye, TournamentRound nextRound) {}
