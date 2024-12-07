b64437becec4c6f44b1b0cac6ba7ffe8a80e9a68
allgaleano <all.galeano.diaz@gmail.com>
2024-12-07 13:32:03 +0100

    refactor: simplified matches sets and games to avoid excessive joins

--- back/src/main/java/es/upm/tennis/tournament/manager/repo/SetResultRepository.java
+++
@@ -1,7 +0,0 @@
-package es.upm.tennis.tournament.manager.repo;
-
-import es.upm.tennis.tournament.manager.model.SetResult;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SetResultRepository extends JpaRepository<SetResult, Long> {
-}
