ea8941705a376212fcb9329c6653dcbf3e1b21c7
allgaleano <all.galeano.diaz@gmail.com>
2024-12-09 16:02:28 +0100

    fix/front: fixed match result form with wrapper to close modal and then refresh the page

---
+++ front/components/pages/tournaments/matches/sets/modal-form-wrapper.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { Match } from "@/types";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+interface ChildProps {
+  match: Match;
+  tournamentId: number;
+  onSuccess?: () => void;
+}
+
+interface ModalFormWrapperProps {
+  match: Match;
+  tournamentId: number;
+  variant: "dialog" | "sheet";
+  triggerText?: string;
+  title?: string;
+  children: React.ReactElement<ChildProps>;
+  className?: string;
+}
+
+const ModalFormWrapper = ({
+  match,
+  tournamentId,
+  variant,
+  triggerText = "Anotar Resultados",
+  title,
+  children,
+  className
+}: ModalFormWrapperProps) => {
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+
+  const handleSuccess = () => {
+    setOpen(false);
+
+    setTimeout(() => {
+      router.refresh();
+    }, 500);
+  }
+
+  const childWithProps = React.cloneElement(children, {
+    match,
+    tournamentId,
+    onSuccess: handleSuccess,
+  } as ChildProps);
+
+  const modalTitle = title || `${match.player1?.surname} vs ${match.player2?.surname}`;
+
+  return (
+    <div className={className}>
+      {variant === "dialog" ? (
+        <Dialog open={open} onOpenChange={setOpen}>
+          <DialogTrigger asChild>
+            <Button variant="outline">{triggerText}</Button>
+          </DialogTrigger>
+          <DialogContent className="max-w-xl">
+            <DialogHeader>
+              <DialogTitle>{modalTitle}</DialogTitle>
+            </DialogHeader>
+            <ScrollArea className="max-h-[80vh]">
+              {childWithProps}
+            </ScrollArea>
+          </DialogContent>
+        </Dialog>
+      ) : (
+        <Sheet open={open} onOpenChange={setOpen}>
+          <SheetTrigger asChild>
+            <Button variant="outline">{triggerText}</Button>
+          </SheetTrigger>
+          <SheetContent side="right" className="bg-white">
+            <SheetHeader>
+              <SheetTitle>{modalTitle}</SheetTitle>
+            </SheetHeader>
+            <ScrollArea className="h-[90vh]">
+              {childWithProps}
+            </ScrollArea>
+          </SheetContent>
+        </Sheet>
+      )}
+    </div>
+  );
+}
+
+export default ModalFormWrapper;
