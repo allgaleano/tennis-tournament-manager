70fcc11c2de24c84915387d2cd921ee7b488b0f3
allgaleano <all.galeano.diaz@gmail.com>
2024-12-10 11:32:20 +0100

    fix/back: fixed duplicates for tournament participations

--- back/src/main/java/es/upm/tennis/tournament/manager/repo/TournamentParticipationRepository.java
+++ back/src/main/java/es/upm/tennis/tournament/manager/repo/TournamentParticipationRepository.java
@@ -1,9 +1,14 @@
 package es.upm.tennis.tournament.manager.repo;
 
+import es.upm.tennis.tournament.manager.model.PlayerStats;
+import es.upm.tennis.tournament.manager.model.Tournament;
 import es.upm.tennis.tournament.manager.model.TournamentParticipation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TournamentParticipationRepository extends JpaRepository<TournamentParticipation, Long> {
+    Optional<TournamentParticipation> findByTournamentAndPlayerStats(Tournament tournament, PlayerStats playerStats);
 }
